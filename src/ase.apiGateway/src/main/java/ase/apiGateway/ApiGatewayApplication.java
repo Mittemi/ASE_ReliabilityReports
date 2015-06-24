151770855762329d66a903286fcd0cce0edbd659
Michael Mittermayr <e1126749@student.tuwien.ac.at>
2015-06-24 18:29:51 +0200

    code fix

--- src/ase.apiGateway/src/main/java/ase/apiGateway/ApiGatewayApplication.java
+++ src/ase.apiGateway/src/main/java/ase/apiGateway/ApiGatewayApplication.java
@@ -1,14 +1,13 @@
 package ase.apiGateway;
 
 import ase.shared.ASEModelMapper;
-import ase.shared.HelperBeans;
 import ase.shared.commands.CommandFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "ase.apiGateway" }, basePackageClasses = {CommandFactory.class, HelperBeans.class})
+@ComponentScan(basePackages = { "ase.apiGateway" }, basePackageClasses = {CommandFactory.class, ASEModelMapper.class})
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
