eaef9c5058a15c95f58772236f1e1847c92057b8
Michael Mittermayr <e1126749@student.tuwien.ac.at>
2015-06-25 16:53:52 +0200

    Hystrix only on profile "productive"

--- src/ase.apiGateway/src/main/java/ase/apiGateway/config/HystrixAspectConfig.java
+++ src/ase.apiGateway/src/main/java/ase/apiGateway/config/HystrixAspectConfig.java
@@ -3,11 +3,14 @@ package ase.apiGateway.config;
 import com.netflix.hystrix.contrib.javanica.aop.aspectj.HystrixCommandAspect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by Michael on 23.06.2015.
  */
 @Configuration
+//enable hystrix only if running with profile productive
+@Profile(value = {"productive"})
 public class HystrixAspectConfig {
 
     @Bean
